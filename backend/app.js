277d06f97e6edbfd05a340e5896f412a1e8fb1e8
Ritesh Raj Moond <moondritesh@gmail.com>
2024-09-17 17:17:41 +0530

    improved login/signup

--- backend/app.js
+++ backend/app.js
@@ -22,8 +22,6 @@ const {
   uploadDeveloperZip,
 } = require("./config/cloudConfig");
 
-const Developer = require("./models/Developer");
-
 mongoose.connect("mongodb://127.0.0.1:27017/dummy");
 
 let app = express();
