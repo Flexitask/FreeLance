25ddadd46c2fd5c1107c7b042b88d955623be3cd
Divyam Verma <divyamvbij@gmail.com>
2024-09-17 15:09:39 +0530

    developer profile

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,5 +1,4 @@
 const mongoose=require("mongoose");
-const Developer = require("./Developer");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
@@ -8,12 +7,6 @@ const CategorySchema = new Schema({
        required: true,
        enum: ["Front-end", "Back-end", "Logo-design","UI/UX", "Machine Learning","Web-application"],
     },
-    clients : [
-        {
-            type: Schema.Types.ObjectId,
-            ref : "Client",
-        }
-    ],
     Developer : [
         {
             type: Schema.Types.ObjectId,
